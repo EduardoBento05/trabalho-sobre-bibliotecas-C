3edb85e132d8b5823680e5dc484ce1f0d633a50a
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2021-04-20 12:13:50 -0300

    Add files via upload

---
+++ minhabiblio.h
@@ -0,0 +1,14 @@
+/***************************************************************
+Nomes:  Eduardo de Almeida Bento Dias   Emanuel de Almeida Souza
+RAs:    2319152                         2317915
+***************************************************************/
+
+#ifndef MINHABIBLIO_H
+#define MINHABIBLIO_H
+
+int Fatorial(int n);
+int Potencia(int base, int exp);
+int Fibonacci(int n);
+float MediaAltura(int n);
+
+#endif
